b3f56bf94440c379f299c33d81a9632bbc5835ea
kaushik113 <kaushik.kaushik@iitgn.ac.in>
2026-07-11 15:27:49 +0530

    Data updated

--- demo.py
+++ demo.py
@@ -10,12 +10,12 @@ from src.logger import logging
 # --------------------------------------------------------------------------------
 
 # # below code is to check the exception config
-from src.logger import logging
-from src.exception import MyException
-import sys
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1+2
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = 1+2
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
