b6cc8041f6d247a8ea6f74c5273adfd29ff838e6
kaushik113 <kaushik.kaushik@iitgn.ac.in>
2026-07-11 10:05:01 +0530

    structure ready & venv is set to mlopsproject

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Kaushik",
+    author_email="kaushik21work@gmail.com",
+    packages=find_packages()
+)
