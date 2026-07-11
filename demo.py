813c40973582e7350d86eb4e71d68b98ee9ac526
kaushik113 <kaushik.kaushik@iitgn.ac.in>
2026-07-11 17:37:17 +0530

    dataIngestionsetuplol

--- demo.py
+++ demo.py
@@ -19,3 +19,10 @@ from src.logger import logging
 # except Exception as e:
 #     logging.info(e)
 #     raise MyException(e, sys) from e
+
+# --------------------------------------------------------------------------------
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
