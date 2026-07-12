6de49be135e41f0ebacce9835106d9d8925664cf
kaushik113 <kaushik.kaushik@iitgn.ac.in>
2026-07-12 08:46:07 +0530

    data_transformation_added_

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -42,7 +42,7 @@ DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_REPORT_FILE_NAME: str = "report.yaml"
 
 """
-Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
+3 . Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
 """
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
